cdfdedf36fe5a78c75453d0d2648f5822d5cead3
Maxim Bortnikov | מקסים בורטניקוב <43994704+Northstrix@users.noreply.github.com>
2025-03-18 14:01:51 +0700

    Uploaded V1.2.5 with 51 components

---
+++ next-i18next.config.js
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: require('./public/locales/en/translation.json'), // English translation
+      },
+      he: {
+        translation: require('./public/locales/he/translation.json'), // Hebrew translation
+      },
+    },
+    lng: 'en', // Default language
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false, // React already does escaping
+    },
+  });
+
+export default i18n;
